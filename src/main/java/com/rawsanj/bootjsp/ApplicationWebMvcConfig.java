de4804d61366ceb24a024892b335b686f37ab5ee
leogtzr <leogutierrezramirez@gmail.com>
2016-10-09 22:41:40 -0600

    Changing greeting mapping

--- src/main/java/com/rawsanj/bootjsp/ApplicationWebMvcConfig.java
+++ src/main/java/com/rawsanj/bootjsp/ApplicationWebMvcConfig.java
@@ -1,26 +1,29 @@
 package com.rawsanj.bootjsp;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class ApplicationWebMvcConfig extends WebMvcConfigurerAdapter{
+public class ApplicationWebMvcConfig extends WebMvcConfigurerAdapter {
 
+	private static final String PREFIX = "/WEB-INF/pages/";
+	private static final String SUFFIX = ".jsp";
+	
     @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
     
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/pages/");
-        resolver.setSuffix(".jsp");
+        resolver.setPrefix(PREFIX);
+        resolver.setSuffix(SUFFIX);
         return resolver;
     }
 }
