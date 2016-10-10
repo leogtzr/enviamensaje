dc5421912cd298420206867f61e34fa0f89542e9
leogtzr <leogutierrezramirez@gmail.com>
2016-10-09 18:47:04 -0600

    Test 4

--- src/main/java/com/enviamensaje/app/Main.java
+++ src/main/java/com/enviamensaje/app/Main.java
@@ -1,5 +1,10 @@
 package com.enviamensaje.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Main {
 	public static void main(final String[] args) {
 	}
